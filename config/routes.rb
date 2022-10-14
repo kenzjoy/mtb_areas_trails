6c0fc594902c8cf05533f133a376c7a770909ea4
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-14 12:55:57 -0600

    feat: add trail id route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/areas', to: 'areas#index'
   get '/areas/:id', to: 'areas#show'
   get '/trails', to: 'trails#index'
+  get '/trails/:id', to: 'trails#show'
 end
