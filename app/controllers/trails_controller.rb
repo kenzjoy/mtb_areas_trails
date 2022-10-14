92c7a1dc524a77d4dac4ddf8857775011e56557a
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-14 12:40:34 -0600

    feat: user story 3 passing test and rendered index

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,3 +1,5 @@
 class TrailsController < ApplicationController
-  
+  def index
+    @trails = Trail.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 @gulch = Area.create!(name: "Horse Gulch", region: "Durango, CO", peak_elevation: 7900, alpine: false)
-@telegraph = @gulch.trails.create!(name: "Telegraph", difficulty: "Blue", rating: 4.0, length: 2.8, open: true)
-@anasazi = @gulch.trails.create!(name: "Anasazi", difficulty: "Black", rating: 5.0, length: 0.7, open: true)
-
 @hermosa = Area.create!(name: "Hermosa Creek", region: "San Juan South", peak_elevation: 10750, alpine: true)
-@dutch = @hermosa.trails.create!(name: "Dutch Creek", difficulty: "Black", rating: 5.0, length: 5.9, open: true)
-@goulding = @hermosa.trails.create!(name: "Goulding Creek", difficulty: "Double Black", rating: 3.2, length: 2.9, open: true)
+
+@telegraph = Trail.create!(name: "Telegraph", difficulty: "Blue", rating: 4.0, length: 2.8, open: true)
+@anasazi = Trail.create!(name: "Anasazi", difficulty: "Black", rating: 5.0, length: 0.7, open: true)
+
+@dutch = Trail.create!(name: "Dutch Creek", difficulty: "Black", rating: 5.0, length: 5.9, open: true)
+@goulding = Trail.create!(name: "Goulding Creek", difficulty: "Double Black", rating: 3.2, length: 2.9, open: true)
 
