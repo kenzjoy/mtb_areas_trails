03b0645cba92d8287ea6b7c380b7299872fd7754
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-13 22:27:47 -0600

    feat: update conventions on areas show spec

--- spec/features/areas/show_spec.rb
+++ spec/features/areas/show_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper' 
 
-RSpec.describe 'the areas show page' do
+RSpec.describe '/areas/show.html.erb' do
   before(:each) do
     @gulch = Area.create!(name: "Horse Gulch", region: "Durango, CO", peak_elevation: 7900, alpine: false)
     @hermosa = Area.create!(name: "Hermosa Creek", region: "San Juan South", peak_elevation: 10750, alpine: true)
   end
 
-  describe 'when a user visits /areas/:id page' do
-    it 'shows the area tied to the specified :id and its attributes' do
-      visit "/areas/#{@gulch.id}"
+  describe 'as a user' do
+    describe 'when I visit /areas/:id' do
+      it 'shows the area tied to the specified :id and its attributes' do
+        visit "/areas/#{@gulch.id}"
 
-      expect(page).to have_content(@gulch.region)
-      expect(page).to have_content(@gulch.peak_elevation)
-      expect(page).to have_content(@gulch.alpine)
-      expect(page).to_not have_content(@hermosa.region)
+        expect(page).to have_content(@gulch.region)
+        expect(page).to have_content(@gulch.peak_elevation)
+        expect(page).to have_content(@gulch.alpine)
+        expect(page).to_not have_content(@hermosa.region)
+      end
     end
   end
 end
