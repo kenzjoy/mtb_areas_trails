9accfe02310e5aa6a71537f9c76d2b6d39cf23d6
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-14 17:13:44 -0600

    feat: add area id to seeds and instance variables in test

--- app/controllers/area_trails_controller.rb
+++ app/controllers/area_trails_controller.rb
@@ -1,3 +1,5 @@
 class AreaTrailsController < ApplicationController
-  
+  def index
+    
+  end
 end
---
+++ spec/features/areas/trails/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe '/areas/area_id/trails/index.html.erb', type: :feature do
+  before(:each) do
+    @gulch = Area.create!(name: "Horse Gulch", region: "Durango, CO", peak_elevation: 7900, alpine: false)
+    @hermosa = Area.create!(name: "Hermosa Creek", region: "San Juan South", peak_elevation: 10750, alpine: true)
+    
+    @telegraph = Trail.create!(name: "Telegraph", difficulty: "Blue", rating: 4.0, length: 2.8, open: true, area_id: @gulch.id)
+    @anasazi = Trail.create!(name: "Anasazi", difficulty: "Black", rating: 5.0, length: 0.7, open: true, area_id: @gulch.id)   
+    @dutch = Trail.create!(name: "Dutch Creek", difficulty: "Black", rating: 5.0, length: 5.9, open: true, area_id: @hermosa.id)
+    @goulding = Trail.create!(name: "Goulding Creek", difficulty: "Double Black", rating: 3.2, length: 2.9, open: true, area_id: @hermosa.id)
+  end
+
+  describe 'as as user' do
+    describe 'when I visit the area_id/trails index' do
+      it 'displays each trail that is associated with that area along with the trail attributes' do
+
+      end
+    end
+  end
+end
